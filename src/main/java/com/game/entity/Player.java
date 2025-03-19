7ea7aaa85906ea5751d9f57bbbf07062353a824e
Buga Dan <danbil.942@gmail.com>
2025-03-19 22:00:01 +0200

    First commit with modiffications in Player class and added SessionFactory in PlayerRepositoryDB.java . Ne dependencies in pom.xml . Now is possible to build the project

--- src/main/java/com/game/entity/Player.java
+++ src/main/java/com/game/entity/Player.java
@@ -1,23 +1,37 @@
 package com.game.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 import java.util.Date;
 
-
+@Entity
+@Table(name = "player" , schema = "rpg")
 public class Player {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @Column(name = "name" , length = 12 , nullable = false)
     private String name;
-
+    @Column(name = "title" , length = 30 , nullable = false)
     private String title;
-
+    @Enumerated(EnumType.ORDINAL)
+    @Column(name = "race" , nullable = false)
     private Race race;
-
+    @Enumerated(EnumType.ORDINAL)
+    @Column(name = "profession" , nullable = false)
     private Profession profession;
-
+    @Column(name = "birthday" , nullable = false)
     private Date birthday;
-
+    @Column(name = "banned" , nullable = false)
     private Boolean banned;
-
+    @Column(name = "level" , nullable = false)
     private Integer level;
 
     public Player() {
